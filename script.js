9a6e56aa944217c7a3040f2008e2e3611ccf6228
williamfisher9 <98424965+williamfisher9@users.noreply.github.com>
2024-07-14 00:43:34 -0500

    use offset values to place object

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const item = document.getElementById("item");
 
 let mousePressed = false;
+let itemGrabXVal = 0;
+let itemGrabYVal = 0;
 
 item.addEventListener("mousedown", () => {
     mousePressed = true;
+    
+    itemGrabXVal = event.offsetX;
+    itemGrabYVal = event.offsetY;
+    
     console.log(`down ${mousePressed}`)
 });
 
@@ -14,19 +20,17 @@ item.addEventListener("mousemove", (event) => {
     else {
         console.log(`moving ${mousePressed}`)
 
-        let itemHalfWidth = item.getBoundingClientRect().width/2;
-        let itemHalfHeight = item.getBoundingClientRect().height/2;
-
         let xVal = event.clientX;
         let yVal = event.clientY;
 
-        item.style.left = `${xVal-itemHalfWidth}px`;
-        item.style.top = `${yVal-itemHalfHeight}px`;
-
+        item.style.left = `${xVal-itemGrabXVal}px`;
+        item.style.top = `${yVal-itemGrabYVal}px`;
     }
 });
 
 item.addEventListener("mouseup", () => {
     mousePressed = false;
+    itemGrabXVal = 0;
+    itemGrabYVal = 0;
     console.log(`up ${mousePressed}`)
 });
